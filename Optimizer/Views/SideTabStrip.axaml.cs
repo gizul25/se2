42793cefe13c454b51ebbdb61154d4b2ac190c34
gizul25 <gizul25@student.sdu.dk>
2026-03-12 12:29:24 +0000

    implement gui navigation layout

---
+++ Optimizer/Views/SideTabStrip.axaml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace SE2.Views;
+
+public partial class SideTabStrip : UserControl
+{
+    Panel? injectorNode;
+    List<ScenarioNav> scenarios = [];
+
+    public SideTabStrip()
+    {
+        InitializeComponent();
+
+        TabNode.SelectionChanged += TabNode_SelectionChanged;
+    }
+
+    public void SetInjectorNode(Panel? injectorNode)
+    {
+        this.injectorNode = injectorNode;
+    }
+
+    public void AddScenario(string name, ScenarioNav instance)
+    {
+        scenarios.Add(instance);
+
+        var tabItem = new TabStripItem
+        {
+            Content = name,
+        };
+        tabItem.Classes.Add("SidePanel");
+        TabNode.Items.Add(tabItem);
+    }
+
+    public void TabNode_SelectionChanged(object sender, RoutedEventArgs e)
+    {
+        SelectIndex(TabNode.SelectedIndex);
+    }
+
+    public void SelectIndex(int index)
+    {
+        if (index < 0 || index >= scenarios.Count)
+        {
+            return;
+        }
+
+        injectorNode?.Children.Clear();
+        injectorNode?.Children.Add(scenarios[index]);
+    }
+}
