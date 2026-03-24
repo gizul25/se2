c0b8c3edaa8228d29914a4f9f33270c5d069f88a
DTL6 <daavr25@student.sdu.dk>
2026-03-24 23:13:18 +0100

    Added Unit Tests

--- Tests/SE2.Test.cs
+++ Tests/SE2.Test.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using SE2.Data;
+using SE2.Domain;
 
 namespace SE2.Test
 {
@@ -44,6 +45,66 @@ namespace SE2.Test
             Assert.Throws<Exception>(() => rdm.Save("not existing"));
             Assert.Pass();
         }
-    }
-}
 
+        
+    }
+    
+    public class OptimizerTests
+        {
+            private Optimizer CreateValidOptimizer()
+            {
+                return new Optimizer
+                {
+                    Sources = new List<SourceData>
+                    {
+                        new SourceData { StartTime = DateTime.Today, HeatDemand = 5, ElectricityPrice = 100 }
+                    },
+                    Assets = new List<Asset>
+                    {
+                        new Asset { Name = "Asset1", MaxHeat = 10, MaxElectricity = 20, ProductionCosts = 50, Image = "test.jpg" }
+                    }
+                };
+            }
+
+            [Test]
+            public void LoadNotExistingAsset()
+            {
+                var optimizer = CreateValidOptimizer();
+                optimizer.Assets.Add(null!);
+                Assert.Throws<Exception>(() => optimizer.OptimizerInit());
+            }
+            
+            [Test]
+            public void CalculateSchedule_WithInsufficientAssets()
+            {
+                var optimizer = CreateValidOptimizer();
+                optimizer.Assets.Clear();
+                Assert.Throws<Exception>(() => optimizer.calculateSchedule());
+            }
+            
+            [Test]
+            public void LoadExists()
+            {
+                var optimizer = CreateValidOptimizer();
+                Assert.DoesNotThrow(() => optimizer.OptimizerInit());
+            }
+
+            [Test]
+            public void CalculateSchedule_WithCorrectAssets()
+            {
+                var optimizer = CreateValidOptimizer();
+                optimizer.OptimizerInit();
+                var result = optimizer.calculateSchedule();
+                Assert.IsNotNull(result);
+                Assert.IsNotEmpty(result);
+            }
+
+            [Test]
+            public void CalculateNetCosts_WithEmptyAssets()
+            {
+                var optimizer = CreateValidOptimizer();
+                optimizer.Assets = new List<Asset>();
+                Assert.Throws<Exception>(() => optimizer.calculateNetCost());
+            }
+        }
+}
