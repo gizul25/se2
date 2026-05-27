1a3e165923433d44ce0e0de6001d3dd23c3f872f
DTL6 <daavr25@student.sdu.dk>
2026-05-27 18:25:22 +0200

    Heat Demand color changed to black

--- Optimizer/Utils/GraphUtils.cs
+++ Optimizer/Utils/GraphUtils.cs
@@ -17,6 +17,7 @@ public class GraphUtils
     public static SKColor BrightRed = new SKColor(237, 7, 27);
     public static SKColor CherryRed = new SKColor(142, 0, 12);
     public static SKColor BrightGreen = new SKColor(34, 177, 76);
+	public static SKColor Black = new SKColor(0, 0, 0);
 
     public static ISeries Series(string name, IReadOnlyCollection<DateTimePoint>? values, SKColor color)
     {
