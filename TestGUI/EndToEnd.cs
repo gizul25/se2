5f64deb0da07010b374dcede0274e037161e9ed0
nkris125 <nkris25@student.sdu.dk>
2026-05-31 16:20:26 +0100

    EndtoEnd fixes
    Refactor end-to-end tests to improve structure and add new scenarios.

--- TestGUI/EndToEnd.cs
+++ TestGUI/EndToEnd.cs
@@ -1,120 +1,179 @@
-using System.Globalization;
-using System.Reflection;
+using System;
 using System.Linq;
-using Avalonia;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Headless;
 using Avalonia.Headless.XUnit;
-using Avalonia.Input;
-using Avalonia.Controls.Primitives;
-using LiveChartsCore.Defaults;
-using SE2.Utils;
+using CommunityToolkit.Mvvm.Input;
 using SE2.Views;
 using SE2.ViewModels;
-using SE2.Models;
-using SE2.Data;
 using SE2.Domain;
-using SE2;
+using SE2.Data;
 using Xunit;
 
-namespace EndToEnd
+namespace SE2.Headless.XUnit;
+
+public class EndToEndTests
 {
-    public class EndToEndTests
+    public EndToEndTests()
     {
-        [AvaloniaFact]
-        public void Positive_MainWindow_OpensAndShowsScenarioNavigation()
+        DM.Init();
+        DM.StartOptimizer();
+    }
+
+    
+    
+    [AvaloniaFact]
+    public async Task Positive_FullFlow_SelectScenario_Period_RunOptimizer_CheckCharts()
+    {
+        var window = new MainWindow();
+        window.Show();
+
+        var scenarioNav = GetFirstScenarioNav(window);
+
+        
+        var scenarioList = scenarioNav.FindControl<ListBox>("ScenarioList");
+        Assert.NotNull(scenarioList);
+        scenarioList.SelectedIndex = 0;
+
+        
+        var periodCombo = scenarioNav.FindControl<ComboBox>("PeriodComboBox");
+        Assert.NotNull(periodCombo);
+        periodCombo.SelectedItem = "Winter period";
+
+        
+        var optimizerView = GetOptimizerView(window);
+
+        
+        var optimizerBtn = optimizerView.FindControl<Button>("OptimizerBtn");
+        Assert.NotNull(optimizerBtn);
+
+        var cmd = optimizerBtn.Command as IAsyncRelayCommand;
+        Assert.NotNull(cmd);
+
+        await cmd!.ExecuteAsync(null);
+
+    
+        var vm = optimizerView.DataContext as OptimizerViewModel;
+        Assert.NotNull(vm);
+
+        Assert.True(vm!.ExportEnabled);
+        Assert.True(vm.Charts.Count >= 5);
+
+        foreach (var chart in vm.Charts)
+            Assert.NotEmpty(chart.Series);
+
+        Assert.NotEqual(0, vm.TotalCost);
+        Assert.NotEqual(0, vm.HeatProduced);
+    }
+
+    [AvaloniaFact]
+    public async Task Positive_ChangePeriod_ReloadsCharts()
+    {
+        var window = new MainWindow();
+        window.Show();
+
+        var scenarioNav = GetFirstScenarioNav(window);
+
+        var periodCombo = scenarioNav.FindControl<ComboBox>("PeriodComboBox");
+        Assert.NotNull(periodCombo);
+
+        periodCombo.SelectedItem = "Winter period";
+
+        var optimizerView = GetOptimizerView(window);
+        var vm = optimizerView.DataContext as OptimizerViewModel;
+
+        var optimizerBtn = optimizerView.FindControl<Button>("OptimizerBtn");
+        var cmd = optimizerBtn.Command as IAsyncRelayCommand;
+
+        await cmd!.ExecuteAsync(null);
+
+        var initialCount = vm!.Charts.Count;
+
+        
+        periodCombo.SelectedItem = "Summer period";
+
+        await cmd.ExecuteAsync(null);
+
+        Assert.True(vm.Charts.Count == initialCount);
+        foreach (var chart in vm.Charts)
+            Assert.NotEmpty(chart.Series);
+    }
+
+    
+    [Fact]
+    public void Negative_InvalidScenario_Throws()
+    {
+        var loader = new ScenarioLoader();
+        Assert.ThrowsAny<Exception>(() => loader.Load("999"));
+    }
+
+
+    [Fact]
+    public async Task Negative_OptimizerWithoutAssets_Throws()
+    {
+        DM.Init();
+        DM.AM.Assets.Clear();
+
+        var vm = new OptimizerViewModel();
+        var cmd = vm.RunOptimizationCommand;
+
+        await Assert.ThrowsAnyAsync<Exception>(async () =>
         {
-            SE2.Domain.DM.Init();
-            SE2.Domain.DM.StartOptimizer();
+            await cmd.ExecuteAsync(null);
+        });
+    }
 
-            var window = new MainWindow();
-            window.Show();
+    
 
-            var scenarioNav = GetFirstScenarioNav(window);
+    [Fact]
+    public void Edge_DefaultPeriod_IsWinter()
+    {
+        var vm = new ScenarioNavViewModel();
+        Assert.Equal("Winter period", vm.SelectedPeriod);
+    }
 
-            Assert.NotNull(scenarioNav);
-            Assert.NotNull(scenarioNav.FindControl<TopTabStrip>("TopTabStripNode"));
-        }
+    [Fact]
+    public void Edge_ChartsEmpty_WhenNoResults()
+    {
+        DM.Init();
+        DM.RDM.SetCurrentScenarioResultingData(null!);
 
-        [AvaloniaFact]
-        public void Positive_OptimizerButton_CommandCanExecute()
-        {
-            SE2.Domain.DM.Init();
-            SE2.Domain.DM.StartOptimizer();
+        var vm = new OptimizerViewModel();
 
-            var window = new MainWindow();
-            window.Show();
+        Assert.Empty(vm.Charts);
+        Assert.False(vm.ExportEnabled);
+    }
 
-            var optimizerView = GetOptimizerView(window);
-            var optimizerBtn = optimizerView.FindControl<Button>("OptimizerBtn");
+   
 
-            Assert.NotNull(optimizerBtn);
-            Assert.NotNull(optimizerBtn!.Command);
+    private static ScenarioNav GetFirstScenarioNav(MainWindow window)
+    {
+        var injectorNode = window.FindControl<Panel>("InjectorNode");
+        Assert.NotNull(injectorNode);
 
-            var exception = Record.Exception(() => optimizerBtn.Command.Execute(null));
+        var scenarioNav = injectorNode!.Children.OfType<ScenarioNav>().FirstOrDefault();
+        Assert.NotNull(scenarioNav);
 
-            Assert.Null(exception);
-        }
+        return scenarioNav!;
+    }
 
-        [Fact]
-        public void Negative_InvalidScenarioId_ThrowsFileNotFoundException()
-        {
-            var scenarioLoader = new ScenarioLoader();
+    private static OptimizerView GetOptimizerView(MainWindow window)
+    {
+        var scenarioNav = GetFirstScenarioNav(window);
 
-            Assert.ThrowsAny<Exception>(() => scenarioLoader.Load("999"));
-        }
+        var topTabStrip = scenarioNav.FindControl<TopTabStrip>("TopTabStripNode");
+        var scenarioInjector = scenarioNav.FindControl<Panel>("InjectorNode");
 
-        [Fact]
-        public void Negative_OptimizerWithoutAssets_ThrowsException()
-        {
-            var optimizer = new Optimizer();
+        Assert.NotNull(topTabStrip);
+        Assert.NotNull(scenarioInjector);
 
-            Assert.ThrowsAny<Exception>(() => optimizer.CalculateSchedule());
-        }
+        topTabStrip!.SelectIndex(1);
 
-        [Fact]
-        public void Edge_DefaultScenarioPeriod_IsWinter()
-        {
-            var viewModel = new ScenarioNavViewModel();
+        var optimizerView = scenarioInjector!.Children.OfType<OptimizerView>().FirstOrDefault();
+        Assert.NotNull(optimizerView);
 
-            Assert.Equal("Winter period", viewModel.SelectedPeriod);
-        }
-
-        [Fact]
-        public void Edge_ChartControlViewModel_DefaultSeries_IsEmpty()
-        {
-            var viewModel = new ChartControlViewModel();
-
-            Assert.NotNull(viewModel.Series);
-            Assert.Empty(viewModel.Series);
-        }
-
-        private static ScenarioNav GetFirstScenarioNav(MainWindow window)
-        {
-            var injectorNode = window.FindControl<Panel>("InjectorNode");
-            Assert.NotNull(injectorNode);
-
-            var scenarioNav = injectorNode!.Children.OfType<ScenarioNav>().FirstOrDefault();
-            Assert.NotNull(scenarioNav);
-
-            return scenarioNav!;
-        }
-
-        private static OptimizerView GetOptimizerView(MainWindow window)
-        {
-            var scenarioNav = GetFirstScenarioNav(window);
-            var topTabStrip = scenarioNav.FindControl<TopTabStrip>("TopTabStripNode");
-            var scenarioInjector = scenarioNav.FindControl<Panel>("InjectorNode");
-
-            Assert.NotNull(topTabStrip);
-            Assert.NotNull(scenarioInjector);
-
-            topTabStrip!.SelectIndex(1);
-
-            var optimizerView = scenarioInjector!.Children.OfType<OptimizerView>().FirstOrDefault();
-            Assert.NotNull(optimizerView);
-
-            return optimizerView!;
-        }
+        return optimizerView!;
     }
 }
